5af8dea9ecfb3200be937f2ce78d3c9bf242570f
Veysel Erden <12845347+veyselerden@users.noreply.github.com>
2022-03-03 01:52:39 +0300

    script quality and some papercuts

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo apt install ^yaru.* git
+sudo apt install yaru-theme-gtk yaru-theme-unity git
 sudo rm -R /usr/share/themes/Yaru/unity
 git clone https://github.com/veyselerden/yaru-theme-unity.git
 cd yaru-theme-unity
@@ -10,12 +10,17 @@ sudo ln -s /usr/share/themes/Yaru/unity /usr/share/themes/Yaru-light/unity
 
 echo "Installation has finished. Now open Unity Tweak Tool and choose or re-choose Yaru variation you want."
 
-sleep 2
+sleep 1
 
 echo "If Yaru theme not shown in Unity Tweak tool do these steps: https://github.com/ubuntu/yaru/issues/1571#issuecomment-671341217"
 
-sleep 2
+sleep 1
 
+echo "This windows will auto-close itself in 5 seconds"
+
+sleep 5
+
+exit
 
 
 
